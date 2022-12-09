2ccf13c9e61f88bc20ee5ee53ac0bec393d1851a
Wystan Lin <weijlin850621@gmail.com>
2022-12-10 02:51:43 +0800

    重新編譯結果

---
+++ assets/collect-41c42aef.js
@@ -0,0 +1,14 @@
+import{i as d,c as r,k as h,h as m,a as u}from"./API-0e45865f.js";import{s as o,a as f,b as g}from"./Notify-628114b5.js";let b={list:[],getList:async function(){h().then(e=>{console.log(e);const{isSuccess:s,message:t,collects:n}=e;if(!s){o(t);return}this.list=n,this.render()})},render:function(){const e=document.querySelector("#spanCnt");e.textContent=this.list.length;const s=document.querySelector("#collectTable > tbody");let t="";this.list.forEach(n=>{const{id:c,name:i,content:a,image:l}=n.Views;t+=`
+                <tr>
+                    <td>
+                        <a href="view.html?id=${c}">${i}</a>
+                    </td>
+                    <td>${a.slice(0,50)}</td>
+                    <td>
+                        <img src="${l}" class="w-100 img-fluid" alt="${i}" width="100">
+                    </td>
+                    <td>
+                        <button class="cancelBtn btn btn-danger" data-id="${c}">取消收藏</button>
+                    </td>
+                </tr>
+            `}),s.innerHTML=t,this.bindEvent()},bindEvent:function(){document.querySelectorAll(".cancelBtn").forEach(async s=>{s.addEventListener("click",async t=>{const n=t.target.dataset.id;f("確定要取消收藏嗎？").then(async c=>{if(c.isConfirmed){let{isSuccess:i,message:a}=await m(n);i?(g(a),this.getList()):o(a)}})})})}};const w=()=>{d(u),r(),b.getList()};w();
---
+++ assets/index-a1bad338.js
@@ -0,0 +1,13 @@
+import{i as l,g as r,a as o}from"./API-0e45865f.js";const d=!1;let h={list:[],getViews:async function(){const s=await r(d),{isSuccess:e,message:i,views:t}=s;e?(this.list=s.views,this.render()):showError(s.message)},render:function(){let s=document.querySelector("#viewList");s.innerHTML="";let e="";this.list.forEach(i=>{const{id:t,name:c,content:a,image:n}=i;e+=`
+                        <div class="col-6 col-md-3">
+                            <div class="card">
+                                <a class="text-decoration-none" href="./view.html?id=${t}">
+                                    <img src="${n}" class="card-img-top" style="height: 200px; max-width: 100%" alt="${c}">
+                                    <div class="card-body">
+                                        <h4 class="text-center card-title h5">${c}</h4>
+                                        <p class="card-text">${a.slice(0,50)}}</p>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    `}),s.innerHTML=e}};const m=()=>{l(o),h.getViews()};m();
