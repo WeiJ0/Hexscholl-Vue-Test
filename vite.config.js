4a936f137f8da6a4298c8a95ae8b1c36c25372c3
Wystan Lin <weijlin850621@gmail.com>
2022-12-10 02:52:02 +0800

    修改配置檔

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  // ...
+  base: 'https://weij0.github.io/Hexscholl-Vue-Test/',
   define: {
     'process.env': {
-        SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZydnliYXNobXF6bW5pdHl2Y2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA1OTQ0NTEsImV4cCI6MTk4NjE3MDQ1MX0.oLJGyqjiw9gssNHBiPBpmu_bC-GH6S7ap03LDQ638FM'
+      SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZydnliYXNobXF6bW5pdHl2Y2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA1OTQ0NTEsImV4cCI6MTk4NjE3MDQ1MX0.oLJGyqjiw9gssNHBiPBpmu_bC-GH6S7ap03LDQ638FM'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        signIn: resolve(__dirname, 'signIn.html'),
+        signUp: resolve(__dirname, 'signUp.html'),
+        view: resolve(__dirname, 'view.html'),
+        editView: resolve(__dirname, 'editView.html'),
+        collect: resolve(__dirname, 'collect.html')
+      }
     }
   }
 })
