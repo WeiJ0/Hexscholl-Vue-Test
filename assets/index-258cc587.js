69a1d4fb26773155637b0ec5c03d4636914362aa
Wystan Lin <weijlin850621@gmail.com>
2022-12-10 15:21:23 +0800

    fixed 首頁說明多一個 }

--- assets/index-258cc587.js
+++ assets/index-258cc587.js
@@ -5,7 +5,7 @@ import{i as l,g as r,a as o}from"./API-424af6de.js";const d=!1;let h={list:[],ge
                                     <img src="${n}" class="card-img-top" style="height: 200px; max-width: 100%" alt="${c}">
                                     <div class="card-body">
                                         <h4 class="text-center card-title h5">${c}</h4>
-                                        <p class="card-text">${a.slice(0,50)}}</p>
+                                        <p class="card-text">${a.slice(0,50)}</p>
                                     </div>
                                 </a>
                             </div>
